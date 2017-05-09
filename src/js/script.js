a5d7c8415c3d69e2d8c5222dcff796192c634201
Alejandro Druetta <aledruetta@gmail.com>
2017-05-09 16:28:54 -0300

    MVC, app() callback

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,39 @@
 var map;
 
-function initMap() {
+function app() {
   "use strict";
 
-  var bounds = new google.maps.LatLngBounds(
-    {lat: -23.532661, lng: -45.230544},
-    {lat: -23.380147, lng: -44.832549}
-  );
-  map = new google.maps.Map(document.getElementById('map'));
-  map.fitBounds(bounds);
+  var model = {
+    markers: [
+      {title: 'Maranduba', position: {lat: -23.532661, lng: -45.230544}},
+      {title: 'Picinguaba', position: {lat: -23.380147, lng: -44.832549}}
+    ]
+  };
+
+  var control = {
+    getMarkers: function() {
+      return model.markers;
+    },
+
+    init: function() {
+      view.init();
+    }
+  };
+
+  var view = {
+    initMap: function() {
+      map = new google.maps.Map(document.getElementById('map'));
+      var markers = control.getMarkers();
+      var maranduba = markers[0];
+      var picinguaba = markers[1];
+      var bounds = new google.maps.LatLngBounds(maranduba.position, picinguaba.position);
+      map.fitBounds(bounds);
+    },
+
+    init: function() {
+      this.initMap();
+    }
+  };
+
+  control.init();
 }
